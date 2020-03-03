92267ae54174d9e7d49ebf93c66c1faf5860884e
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 21:53:42 -0500

    Add files via upload

---
+++ participation_04_3505.c
@@ -0,0 +1,53 @@
+
+//Farhan Shahbaz
+
+//Madison
+
+
+
+#include <stdio.h>
+
+
+
+
+int main()
+{
+
+    int sum = 0;
+
+    int numbers = 0;
+
+    printf("Please enter 10 numbers\n");
+
+    for(int i = 0; i < 10; i++){
+
+        scanf("%d", &numbers);
+
+        sum += numbers;
+    
+}
+    
+    
+	printf("Sum : %d\n", sum);
+
+    	float ab = average(sum);	
+	
+printf("Average of the numbers: %.2f", ab);
+	
+return 0;
+    
+
+}
+
+
+
+//float return type is NOT WORKING FOR SOME REASON
+
+int average(int a){
+
+    float ave = a/10.0;
+
+    //printf("%d")
+    return ave;
+
+}
